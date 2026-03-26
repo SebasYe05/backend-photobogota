7ebaa23489363b835b0f3f6969d08be93e6956cb
Sebastian Sotomayor <sotomayo250525@gmail.com>
2026-03-25 20:51:38 -0500

    Se añadio setup de docker

--- api/src/main/java/com/photobogota/api/controller/AuthController.java
+++ api/src/main/java/com/photobogota/api/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.photobogota.api.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 @Validated
+@CrossOrigin(origins = "http://localhost:5173")
 public class AuthController {
 
     private final IAuthService authService;
